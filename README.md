6ca544e46cd37de70d8aeab0f6366744711277d5
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-11-07 01:08:43 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<div align="center"><h1><em>pofad</em></h1></div>
+
+*Pearls of Functional Algorithms Design* solved by Rust and Clojure.
+
+## License
+
+MIT
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject pofad "0.1.0-SNAPSHOT"
+  :description "Pearls of Functional Algorithm Design"
+  :url "https://github.com/anqurvanillapy/pofad"
+  :license {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
